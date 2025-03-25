d2ebdcf9c425d05a42a3b5fec0020fd7852b977c
Hari Prasath <khariprasath30@gmail.com>
2025-03-25 10:48:26 +0530

    ts chanllenge completed

--- tsrs/src/challange/TypeScript/2ComplexTyping.tsx
+++ tsrs/src/challange/TypeScript/2ComplexTyping.tsx
@@ -1,11 +1,11 @@
 //Typing a  complex State 
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 type PersonType = {
     name: string,
     age: number,
-    date: Date
+    date: string
 }
 
 
@@ -13,12 +13,26 @@ const ComplexTyping = () => {
     const [person, setPerson] = useState<PersonType>({
         name: 'Hari',
         age: 0,
-        date: new Date()
+        date: new Date().toLocaleDateString()
     })
+
+    const updateChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setPerson({ ...person, [name]: value })
+    }
     return <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '12px'
+        }}>
+            <input type="text" value={person.name} onChange={(e) => updateChange(e)} name="name" />
+            <input type="text" value={person.age} onChange={(e) => updateChange(e)} name="age" />
+            <input type='date' value={person.age} onChange={(e) => updateChange(e)} name="date" />
+        </div>
         <h1>{person.name}</h1>
         <h1>{person.age}</h1>
-        <h1>{person.date.toLocaleDateString()}</h1>
+        <h1>{person.date}</h1>
     </div>;
 };
 
--- tsrs/src/challange/TypeScript/3TodoTyping.tsx
+++ tsrs/src/challange/TypeScript/3TodoTyping.tsx
@@ -1,35 +1,68 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 //type a array of todo 
 
 type todoListType = {
     id: number,
-    desc: string
+    desc: string,
+    completed: boolean
 }
 
-
-
-
-
-
 const TodoTyping = () => {
-    const [todo, setTodo] = useState<todoListType[]>([
+    const [todo, setTodo] = useState<string>('')
+    const [todoList, setTodoList] = useState<todoListType[]>([
         {
             id: 1,
-            desc: 'This is new description'
+            desc: 'This is new description',
+            completed: false
         }, {
             id: 2,
-            desc: 'this is a  default Desctription'
+            desc: 'this is a  default Desctription',
+            completed: false
         }
 
     ])
 
+    const addTodo = (): void => {
+        if (todo != '') {
+            setTodoList([...todoList, {
+                id: todoList.length,
+                desc: todo,
+                completed: false
+            }])
+            setTodo('')
+        } else {
+            alert('not to be empty')
+        }
+    }
+
+    const handleCheck = (e: ChangeEvent<HTMLInputElement>, ind: number) => {
+        console.log(e, ind)
+        const isCompleted: boolean = e.target.checked;
+        const newTodoList = todoList.map(val => {
+            if (val.id === ind + 1) {
+                return {
+                    ...val, completed: isCompleted
+                }
+            }
+            return val;
+        })
+        setTodoList(newTodoList)
+
+    }
+
+
     return <div>
+        <div>
+            <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
+            <button onClick={addTodo}>addTodo</button>
+        </div>
         {
-            todo.map((val, ind) => {
+            todoList.map((val, ind) => {
                 return (
                     <div key={ind}>
                         {val.desc}
+                        <input type="checkbox" name="" id="" onChange={(e) => handleCheck(e, ind)} />
                     </div>
                 )
             })
